9d7ea3b9c4d1d5d669a78f1cf2f00a53268ce483
BahramBobojonov <bobojonoffff@gmail.com>
2023-12-24 14:15:01 +0500

    Отменил вызов функции при импорте модуля

--- src/features/square_feature.py
+++ src/features/square_feature.py
@@ -1,8 +1,5 @@
 import pandas as pd
 def area_ratio(data):
     data['area_ratio'] = data['kitchen_area'] / data['area']
-
     return data
 
-
-df = area_ratio(df)# Вызов функции с вашим набором данных
