e0424c5a460a7fcee06600fe68fa7e3da6c806f9
BahramBobojonov <bobojonoffff@gmail.com>
2023-12-24 13:17:08 +0500

    Обновление фичи по площади

--- src/features/square_feature.py
+++ src/features/square_feature.py
@@ -1,12 +1,8 @@
 import pandas as pd
-df = pd.read_csv(r"C:\Users\vivar\PycharmProjects\Russia Real Estate2021\data\external\all_v2.csv")
-def добавить_фичу_отношение_площади(data):
-    data['отношение_площадей'] = data['kitchen_area'] / data['area']
+def area_ratio(data):
+    data['area_ratio'] = data['kitchen_area'] / data['area']
 
     return data
 
 
-df = добавить_фичу_отношение_площади(df)# Вызов функции с вашим набором данных
-
-
-print(df.head())
+df = area_ratio(df)# Вызов функции с вашим набором данных
