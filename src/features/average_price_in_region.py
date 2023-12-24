116bc029a1bbb26106aaabe165617df0c439c322
BahramBobojonov <bobojonoffff@gmail.com>
2023-12-24 15:47:14 +0500

    Отменил вызов функции при импорте модуля

--- src/features/average_price_in_region.py
+++ src/features/average_price_in_region.py
@@ -1,5 +1,31 @@
+import click
 import pandas as pd
-def calculate_average_price_in_region(df, region):
+
+
+def calculate_average_price_in_region_impl(df, region):
     region_data = df[df['region'] == region]
+    region_data = region_data[region_data['price'].notna()]
+
+    if region_data.empty:
+        return None  # Возвращаем None, если нет данных для вычисления среднего значения
+
+    region_data['price'] = region_data['price'].astype(int)
     average_price = region_data['price'].mean()
     return average_price
+
+
+@click.command()
+@click.argument('csv_path', type=click.Path(exists=True))
+@click.argument('region', type=int)
+def calculate_average_price_in_region_cli(csv_path, region):
+    df = pd.read_csv(csv_path)
+    result = calculate_average_price_in_region_impl(df, region)
+
+    if result is not None:
+        print(f"Average Price in Region {region}: {result}")
+    else:
+        print("Unable to calculate average price due to missing values.")
+
+
+if __name__ == '__main__':
+    calculate_average_price_in_region_cli()
--- src/features/filter_data.py
+++ src/features/filter_data.py
@@ -1,8 +1,17 @@
-
+import click
 import pandas as pd
 
-def filter_data(df, min_price=None, max_price=None, min_area=None, max_area=None):
+@click.command()
+@click.argument('csv_path', type=click.Path(exists=True))
+@click.option('--min_price', type=float, help='Minimum price filter')
+@click.option('--max_price', type=float, help='Maximum price filter')
+@click.option('--min_area', type=float, help='Minimum area filter')
+@click.option('--max_area', type=float, help='Maximum area filter')
+def filter_data(csv_path, min_price, max_price, min_area, max_area):
+    df = pd.read_csv(csv_path)
+
     filtered_data = df.copy()
+
     if min_price:
         filtered_data = filtered_data[filtered_data['price'] >= min_price]
     if max_price:
@@ -11,4 +20,10 @@ def filter_data(df, min_price=None, max_price=None, min_area=None, max_area=None
         filtered_data = filtered_data[filtered_data['area'] >= min_area]
     if max_area:
         filtered_data = filtered_data[filtered_data['area'] <= max_area]
-    return filtered_data
+
+    print(f"Arguments: csv_path={csv_path}, min_price={min_price}, max_price={max_price}, min_area={min_area}, max_area={max_area}")
+    print("Filtered Data:")
+    print(filtered_data.head(10))
+
+if __name__ == '__main__':
+    filter_data()
