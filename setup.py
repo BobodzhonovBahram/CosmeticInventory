045573434f73e72580ccbbaec58cf683fa1542b8
BahramBobojonov <bobojonoffff@gmail.com>
2023-12-24 13:32:43 +0500

    Обновлен setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-# setup.py
-
-from setuptools import find_packages, setup
+from setuptools import find_packages
 
 setup(
-    name='src',
-    packages=find_packages() + ['features'],
+    name='sber_ml_est',
+    packages=find_packages(),
     version='0.1.0',
     description='В рамках исследования "Russia Real Estate2021" мы планируем анализировать динамику цен на недвижимость, факторы влияния на спрос и предложение, а также выявлять перспективные регионы для инвестирования. Будем рассматривать ключевые тренды и изменения в структуре рынка, а также оценивать влияние социоэкономических факторов на его развитие.',
     author='Bobojonov Bahram',
